feac1f5fbd536f02e59e4ba6725cfbd1cf04b701
Nigel Rahkola <me@nigelrahkola.com>
2016-10-14 20:56:25 -0700

    Compute cost function of logistic regression

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -19,13 +19,13 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+hypothesis = sigmoid(X * theta);
 
+for i = 1:m
+    J += -y(i,1) * log(hypothesis(i,:)) - (1 - y(i,1)) * log(1 - hypothesis(i,:));
+endfor
 
-
-
-
-
-
+J = 1/m * J;
 
 % =============================================================
 
