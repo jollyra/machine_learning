5ed9f9323616e70ead31e4d121d7ac998fb3f92b
Nigel Rahkola <me@nigelrahkola.com>
2016-10-16 23:31:43 -0700

    Cost func and grad for regularized logistic regression

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,9 +17,33 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+hypothesis = sigmoid(X * theta);
 
+% disp('size of hypothesis')
+% disp(size(hypothesis))
+% disp('size of y')
+% disp(size(y))
+% disp('size of X')
+% disp(size(X))
+% disp('theta')
+% disp(size(theta))
+% disp('lambda')
+% disp(size(lambda))
 
+for i = 1:m
+    J += -y(i,1) * log(hypothesis(i,:)) - (1 - y(i,1)) * log(1 - hypothesis(i,:));
+endfor
+J = 1/m * J + (lambda / (2 * m) * sum(theta(2:end, 1).^2));
 
+temp = zeros(size(grad));
+% Don't regularize theta(1)
+temp(1, 1) = 1/m * sum((hypothesis - y)' * X(:,1));
+% Regularize the rest of the thetas
+for i = 2:length(grad)
+    temp(i, 1) = 1/m * sum((hypothesis - y)' * X(:,i)) + lambda / m * sum(theta(i));
+endfor
+
+grad = temp;
 
 
 % =============================================================
