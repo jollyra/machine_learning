4af791b20c424f455343d2a8cd821d319542bfeb
Nigel Rahkola <me@nigelrahkola.com>
2016-10-22 13:34:22 -0700

    Add one-vs-all classifier prediction

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -18,30 +18,6 @@ ts = size(all_theta)
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should complete the following code to train num_labels
-%               logistic regression classifiers with regularization
-%               parameter lambda.
-%
-% Hint: theta(:) will return a column vector.
-%
-% Hint: You can use y == c to obtain a vector of 1's and 0's that tell use 
-%       whether the ground truth is true/false for this class.
-%
-% Note: For this assignment, we recommend using fmincg to optimize the cost
-%       function. It is okay to use a for-loop (for c = 1:num_labels) to
-%       loop over the different classes.
-%
-%       fmincg works similarly to fminunc, but is more efficient when we
-%       are dealing with large number of parameters.
-%
-% Example Code for fmincg:
-%
-%     [theta] = ...
-%         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
-%                 initial_theta, options);
-
-
 % Set Initial theta
 initial_theta = zeros(n + 1, 1);
 
@@ -54,17 +30,6 @@ for c = 1:num_labels
     [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
     all_theta(c, :) = theta';
 endfor
-
-all_theta = all_theta(:);
-
-
-
-
-
-
-
-
-
 
 % =========================================================================
 
