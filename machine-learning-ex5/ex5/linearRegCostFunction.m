c0a8cfee9c134c2683199688927a338f18a37920
Nigel Rahkola <Nigel Rahkola>
2016-11-04 11:48:56 -0700

    Add cost function with regularization

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -18,16 +18,10 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
-
-
-
-
-
-
-
-
-
-
+hypothesis = X * theta;
+J = 1/(2 * m) * sum((hypothesis - y) .^ 2);
+reg = lambda / (2 * m) * sum(theta(2:end,:) .^ 2);
+J += reg;
 
 
 % =========================================================================
